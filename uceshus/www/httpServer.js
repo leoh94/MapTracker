99ac3182a9b935b586ab9fd6c7d146174eb7db46
Leo Hussain <leo.94@hotmail.co.uk>
2018-04-30 07:12:38 -0400

    Version 1.552 Port Change

--- uceshus/www/httpServer.js
+++ uceshus/www/httpServer.js
@@ -75,7 +75,7 @@ app.use(bodyParser.json());
 	// directly called in a typed URL
 	var http = require('http');
 	var httpServer = http.createServer(app); 
-	httpServer.listen(4480);
+	httpServer.listen(4481);
 	
 	app.get('/',function (req,res) {
 	res.send("hello world from the HTTP server");
