d8f861c595e7446a387aad0a434abfa898a1e824
Leo Hussain <leo.94@hotmail.co.uk>
2018-04-30 15:06:14 -0400

    Work 2nd

--- uceshus/www/httpServer.js
+++ uceshus/www/httpServer.js
@@ -80,7 +80,7 @@ app.use(bodyParser.json());
 	// directly called in a typed URL
 	var http = require('http');
 	var httpServer = http.createServer(app); 
-	httpServer.listen(4482);
+	httpServer.listen(4478);
 	
 	app.get('/',function (req,res) {
 	res.send("hello world from the HTTP server");
